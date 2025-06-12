1279a900da1a7dc826ee113045070dfda9bbb35b
jizong <jizong.peng.1@etsmtl.net>
2025-06-12 17:42:07 -0400

    Refactor argument parsing to use dataclasses for improved configuration management

--- args.py
+++ args.py
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2025, Inria
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
@@ -6,316 +5,225 @@
 # This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
-# For inquiries contact  george.drettakis@inria.fr
+# For inquiries contact george.drettakis@inria.fr
+# Copyright (C) 2025, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
 #
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact george.drettakis@inria.fr
 
-import argparse
+from dataclasses import dataclass, field
+from typing import List, Optional, Literal
+from pathlib import Path
+import tyro
 import os
 
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description="Options for data loading and training"
-    )
+@dataclass
+class DataConfig:
+    """Data and image loading options"""
+    source_path: Path
+    """Path to the data folder (should have sparse/0/ if using COLMAP or evaluating poses)"""
+    images_dir: Path = Path("images")
+    """source_path/images_dir is the path to the images (with extensions jpg, png or jpeg)"""
+    masks_dir: Path = Path("")
+    """If set, source_path/masks_dir is the path to optional masks to apply to the images before computing loss (png)"""
+    num_loader_threads: int = 4
+    """Number of workers to load and prepare input images"""
+    downsampling: float = -1.0
+    """Downsampling ratio for input images"""
+    pyr_levels: int = 2
+    """Number of pyramid levels. Each level l will downsample the image 2^l times in width and height"""
+    min_displacement: float = 0.03
+    """Minimum median keypoint displacement for a new keyframe to be added. Relative to the image width"""
+    start_at: int = 0
+    """Number of frames to skip from the dataset"""
+    sh_degree: int = 3
+    """Spherical harmonics degree"""
 
-    ## Data and Images
-    parser.add_argument(
-        "-s",
-        "--source_path",
-        type=str,
-        required=True,
-        help="Path to the data folder (should have sparse/0/ if using COLMAP or evaluating poses)",
-    )
-    parser.add_argument(
-        "-i",
-        "--images_dir",
-        type=str,
-        default="images",
-        help="source_path/images_dir is the path to the images (with extensions jpg, png or jpeg).",
-    )
-    parser.add_argument(
-        "--masks_dir",
-        type=str,
-        default="",
-        help="If set, source_path/masks_dir is the path to optional masks to apply to the images before computing loss (png).",
-    )
-    parser.add_argument(
-        "--num_loader_threads",
-        type=int,
-        default=4,
-        help="Number of workers to load and prepare input images",
-    )
-    parser.add_argument(
-        "--downsampling",
-        type=float,
-        default=-1.0,
-        help="Downsampling ratio for input images",
-    )
-    parser.add_argument(
-        "--pyr_levels",
-        type=int,
-        default=2,
-        help="Number of pyramid levels. Each level l will downsample the image 2^l times in width and height",
-    )
-    parser.add_argument(
-        "--min_displacement",
-        type=float,
-        default=0.03,
-        help="Minimum median keypoint displacement for a new keyframe to be added. Relative to the image width",
-    )
-    parser.add_argument(
-        "--start_at",
-        type=int,
-        default=0,
-        help="Number of frames to skip from the dataset.",
-    )
 
-    parser.add_argument("--sh_degree", default=3)
+@dataclass
+class ColmapConfig:
+    """COLMAP-related options"""
+    eval_poses: bool = False
+    """Compare poses to COLMAP"""
+    use_colmap_poses: bool = False
+    """Load COLMAP data for pose and intrinsics initialization"""
 
-    ## COLMAP options
-    parser.add_argument(
-        "--eval_poses", action="store_true", help="Compare poses to COLMAP"
-    )
-    parser.add_argument(
-        "--use_colmap_poses",
-        action="store_true",
-        help="Load COLMAP data for pose and intrinsics initialization",
-    )
 
-    ## Learning Rates
-    parser.add_argument(
-        "--lr_poses", type=float, default=1e-4, help="Pose learning rate"
-    )
-    parser.add_argument(
-        "--lr_exposure",
-        type=float,
-        default=5e-4,
-        help="Exposure compensation learning rate",
-    )
-    parser.add_argument("--lr_depth_scale_offset", type=float, default=1e-4)
-    parser.add_argument(
-        "--position_lr_init",
-        type=float,
-        default=0.00005,
-        help="Initial position learning rate",
-    )
-    parser.add_argument(
-        "--position_lr_decay",
-        type=float,
-        default=1 - 2e-5,
-        help="multiplicative decay factor for position learning rate",
-    )
-    parser.add_argument(
-        "--feature_lr", type=float, default=0.005, help="Feature learning rate"
-    )
-    parser.add_argument(
-        "--opacity_lr", type=float, default=0.1, help="Opacity learning rate"
-    )
-    parser.add_argument(
-        "--scaling_lr", type=float, default=0.01, help="Scaling learning rate"
-    )
-    parser.add_argument(
-        "--rotation_lr", type=float, default=0.002, help="Rotation learning rate"
-    )
+@dataclass
+class LearningRateConfig:
+    """Learning rate parameters"""
+    lr_poses: float = 1e-4
+    """Pose learning rate"""
+    lr_exposure: float = 5e-4
+    """Exposure compensation learning rate"""
+    lr_depth_scale_offset: float = 1e-4
+    """Depth scale offset learning rate"""
+    position_lr_init: float = 0.00005
+    """Initial position learning rate"""
+    position_lr_decay: float = 1 - 2e-5
+    """Multiplicative decay factor for position learning rate"""
+    feature_lr: float = 0.005
+    """Feature learning rate"""
+    opacity_lr: float = 0.1
+    """Opacity learning rate"""
+    scaling_lr: float = 0.01
+    """Scaling learning rate"""
+    rotation_lr: float = 0.002
+    """Rotation learning rate"""
 
-    ## Training schedule and losses
-    parser.add_argument(
-        "--lambda_dssim", type=float, default=0.2, help="Weight for DSSIM loss"
-    )
-    parser.add_argument(
-        "--num_iterations",
-        type=int,
-        default=30,
-        help="Number of training iterations per keyframe",
-    )
-    parser.add_argument("--depth_loss_weight_init", type=float, default=1e-2)
-    parser.add_argument(
-        "--depth_loss_weight_decay",
-        type=float,
-        default=0.9,
-        help="Weight decay for depth loss, multiply depth loss weight by this factor every iterations",
-    )
-    parser.add_argument(
-        "--save_at_finetune_epoch",
-        type=int,
-        nargs="+",
-        default=[],
-        help="Enable finetuning after the initial on-the-fly reconstruction and save the scene at the end of the specified epochs when fine-tuning.",
-    )
-    parser.add_argument(
-        "--use_last_frame_proba",
-        type=float,
-        default=0.2,
-        help="Probability of using the last registered frame for each training iteration",
-    )
 
-    ## Pose initialization options
-    # Matching
-    parser.add_argument(
-        "--num_kpts",
-        type=int,
-        default=int(4096 * 1.5),
-        help="Number of keypoints to extract from each image",
-    )
-    parser.add_argument(
-        "--match_max_error",
-        type=float,
-        default=2e-3,
-        help="Maximum reprojection error for matching keypoints, proportion of the image width. This is used to filter outliers and discard points at triangulation.",
-    )
-    parser.add_argument(
-        "--fundmat_samples",
-        type=int,
-        default=2000,
-        help="Maximum number of set of matches used to estimate the fundamental matrix for outlier removal",
-    )
-    parser.add_argument(
-        "--min_num_inliers",
-        type=int,
-        default=100,
-        help="The keyframe will be added only if the number of inliers is greater than this value",
-    )
-    # Initial mini bundle adjustment
-    parser.add_argument(
-        "--num_keyframes_miniba_bootstrap",
-        type=int,
-        default=8,
-        help="Number of first keyframes accumulated for pose and focal estimation before optimization",
-    )
-    parser.add_argument(
-        "--num_pts_miniba_bootstrap",
-        type=int,
-        default=2000,
-        help="Number of keypoints considered for initial mini bundle adjustment",
-    )
-    parser.add_argument("--iters_miniba_bootstrap", type=int, default=200)
-    parser.add_argument("--enable_reboot", action="store_true")
-    # Focal estimation
-    parser.add_argument(
-        "--fix_focal",
-        action="store_true",
-        help="If set, will use init_focal or init_fov without reoptimizing focal",
-    )
-    parser.add_argument(
-        "--init_focal",
-        type=float,
-        default=-1.0,
-        help="Initial focal length in pixels. If not set, will use init_fov or be set as 0.7*width of the image if init_fov is also not set",
-    )
-    parser.add_argument(
-        "--init_fov",
-        type=float,
-        default=-1.0,
-        help="Initial horizontal FoV in degrees. Used only if init_focal is not set",
-    )
-    # Incremental pose optimization
-    parser.add_argument(
-        "--num_prev_keyframes_miniba_incr",
-        type=int,
-        default=6,
-        help="Number of previous keyframes for incremental pose initialization",
-    )
-    parser.add_argument(
-        "--num_prev_keyframes_check",
-        type=int,
-        default=20,
-        help="Number of previous keyframes to check for matches with new keyframe",
-    )
-    parser.add_argument(
-        "--pnpransac_samples",
-        type=int,
-        default=2000,
-        help="Maximum number of set of 2D-3D matches used to estimate the initial pose and outlier removal",
-    )
-    parser.add_argument(
-        "--num_pts_miniba_incr",
-        type=int,
-        default=2000,
-        help="Number of keypoints considered for initial mini bundle adjustment",
-    )
-    parser.add_argument("--iters_miniba_incr", type=int, default=20)
+@dataclass
+class TrainingConfig:
+    """Training schedule and loss parameters"""
+    lambda_dssim: float = 0.2
+    """Weight for DSSIM loss"""
+    num_iterations: int = 30
+    """Number of training iterations per keyframe"""
+    depth_loss_weight_init: float = 1e-2
+    """Initial depth loss weight"""
+    depth_loss_weight_decay: float = 0.9
+    """Weight decay for depth loss, multiply depth loss weight by this factor every iterations"""
+    save_at_finetune_epoch: List[int] = field(default_factory=list)
+    """Enable finetuning after the initial on-the-fly reconstruction and save the scene at the end of the specified epochs when fine-tuning"""
+    use_last_frame_proba: float = 0.2
+    """Probability of using the last registered frame for each training iteration"""
 
-    ## Gaussian initialization options
-    parser.add_argument(
-        "--init_proba_scaler",
-        type=float,
-        default=2,
-        help="Scale the laplacian-based probability of using a pixel to make a new Gaussian primitive. Set to 0 to only use triangulated points.",
-    )
 
-    # Anchor management
-    parser.add_argument(
-        "--anchor_overlap",
-        type=float,
-        default=0.3,
-        help="Size of the overlapping regions when blending between anchors",
-    )
+@dataclass
+class MatchingConfig:
+    """Keypoint matching parameters"""
+    num_kpts: int = int(4096 * 1.5)
+    """Number of keypoints to extract from each image"""
+    match_max_error: float = 2e-3
+    """Maximum reprojection error for matching keypoints, proportion of the image width"""
+    fundmat_samples: int = 2000
+    """Maximum number of set of matches used to estimate the fundamental matrix for outlier removal"""
+    min_num_inliers: int = 100
+    """The keyframe will be added only if the number of inliers is greater than this value"""
 
-    ## Keyframe management
-    parser.add_argument(
-        "--max_active_keyframes",
-        type=int,
-        default=200,
-        help="Maximum number of keyframes to keep in GPU memory. Will start offloading keyframes to CPU if this number is exceeded.",
-    )
 
-    ## Evaluation
-    parser.add_argument(
-        "--test_hold",
-        type=int,
-        default=-1,
-        help="Holdout for test set, will exclude every test_hold image from the Gaussian optimization and use them for testing. The test frames will still be used for training the pose. If set to -1, no keyframes will be excluded from training.",
-    )
-    parser.add_argument(
-        "--test_frequency",
-        type=int,
-        default=-1,
-        help="Test and get metrics every test_frequency keyframes",
-    )
-    parser.add_argument(
-        "--display_runtimes",
-        action="store_true",
-        help="Display runtimes for each step in the tqdm bar",
-    )
+@dataclass
+class MiniBAConfig:
+    """Mini bundle adjustment parameters"""
+    num_keyframes_miniba_bootstrap: int = 8
+    """Number of first keyframes accumulated for pose and focal estimation before optimization"""
+    num_pts_miniba_bootstrap: int = 2000
+    """Number of keypoints considered for initial mini bundle adjustment"""
+    iters_miniba_bootstrap: int = 200
+    """Iterations for initial mini bundle adjustment"""
+    num_prev_keyframes_miniba_incr: int = 6
+    """Number of previous keyframes for incremental pose initialization"""
+    num_prev_keyframes_check: int = 20
+    """Number of previous keyframes to check for matches with new keyframe"""
+    pnpransac_samples: int = 2000
+    """Maximum number of set of 2D-3D matches used to estimate the initial pose and outlier removal"""
+    num_pts_miniba_incr: int = 2000
+    """Number of keypoints considered for initial mini bundle adjustment"""
+    iters_miniba_incr: int = 20
+    """Iterations for incremental mini bundle adjustment"""
 
-    ## Checkpoint options
-    parser.add_argument(
-        "-m",
-        "--model_path",
-        default="",
-        help="Directory to store the renders from test view and checkpoints after training. If not set, will be set to results/xxxxxx.",
-    )
-    parser.add_argument(
-        "--save_every",
-        default=-1,
-        type=int,
-        help="Frequency of exporting renders w.r.t input frames.",
-    )
 
-    ## Viewer
-    parser.add_argument(
-        "--viewer_mode", choices=["local", "server", "web", "none"], default="none"
-    )
-    parser.add_argument(
-        "--ip",
-        type=str,
-        default="0.0.0.0",
-        help="IP address of the viewer client, if using server viewer_mode",
-    )
-    parser.add_argument(
-        "--port",
-        type=int,
-        default=6009,
-        help="Port of the viewer client, if using server viewer_mode",
-    )
+@dataclass
+class FocalConfig:
+    """Focal length estimation parameters"""
+    fix_focal: bool = False
+    """If set, will use init_focal or init_fov without reoptimizing focal"""
+    init_focal: float = -1.0
+    """Initial focal length in pixels"""
+    init_fov: float = -1.0
+    """Initial horizontal FoV in degrees"""
 
-    args = parser.parse_args()
 
-    ## Set the output directory if not specified
-    if args.model_path == "":
-        i = 0
-        while os.path.exists(f"results/{i:06d}"):
-            i += 1
-        args.model_path = f"results/{i:06d}"
+@dataclass
+class GaussianConfig:
+    """Gaussian initialization parameters"""
+    init_proba_scaler: float = 2
+    """Scale the laplacian-based probability of using a pixel to make a new Gaussian primitive"""
+
+
+@dataclass
+class AnchorConfig:
+    """Anchor management parameters"""
+    anchor_overlap: float = 0.3
+    """Size of the overlapping regions when blending between anchors"""
+
+
+@dataclass
+class KeyframeConfig:
+    """Keyframe management parameters"""
+    max_active_keyframes: int = 200
+    """Maximum number of keyframes to keep in GPU memory"""
+
+
+@dataclass
+class EvaluationConfig:
+    """Evaluation parameters"""
+    test_hold: int = -1
+    """Holdout for test set, will exclude every test_hold image from the Gaussian optimization"""
+    test_frequency: int = -1
+    """Test and get metrics every test_frequency keyframes"""
+    display_runtimes: bool = False
+    """Display runtimes for each step in the tqdm bar"""
+
+
+@dataclass
+class CheckpointConfig:
+    """Checkpoint and output parameters"""
+    model_path: Path = Path("")
+    """Directory to store the renders from test view and checkpoints after training"""
+    save_every: int = -1
+    """Frequency of exporting renders w.r.t input frames"""
+
+
+@dataclass
+class ViewerConfig:
+    """Viewer configuration"""
+    viewer_mode: Literal["local", "server", "web", "none"] = "none"
+    """Viewer mode"""
+    ip: str = "0.0.0.0"
+    """IP address of the viewer client, if using server viewer_mode"""
+    port: int = 6009
+    """Port of the viewer client, if using server viewer_mode"""
+
+
+@dataclass
+class Config:
+    """Main configuration for data loading and training"""
+    data: DataConfig
+    colmap: ColmapConfig
+    learning_rates: LearningRateConfig
+    training: TrainingConfig
+    matching: MatchingConfig
+    miniba: MiniBAConfig
+    focal: FocalConfig
+    gaussian: GaussianConfig
+    anchor: AnchorConfig
+    keyframes: KeyframeConfig
+    evaluation: EvaluationConfig
+    checkpoint: CheckpointConfig
+    viewer: ViewerConfig
+    enable_reboot: bool = False
+    """Enable reboot"""
+
+
+def get_args() -> Config:
+    tyro.extras.set_accent_color("red")
+    args = tyro.cli(Config)
+
+    # # Set the output directory if not specified
+    # if str(args.checkpoint.model_path) == "":
+    #     i = 0
+    #     while os.path.exists(f"results/{i:06d}"):
+    #         i += 1
+    #     args.checkpoint.model_path = Path(f"results/{i:06d}")
 
     return args
+
+
+if __name__ == "__main__":
+    args = get_args()
